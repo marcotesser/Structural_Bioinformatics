db18b9e17bd3c822122ca4569267ab4a67205139
marcotesser <marco.tesser.1@studenti.unipd.it>
2022-07-16 16:48:26 +0200

    updates

--- Model.py
+++ Model.py
@@ -2,13 +2,15 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import OneSidedSelection, InstanceHardnessThreshold
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import roc_curve, auc, roc_auc_score
 from sklearn.model_selection import train_test_split, cross_validate, cross_val_score, KFold
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Dropout
 from keras.utils import to_categorical
 from tensorflow.python.framework.random_seed import set_random_seed
 from keras.callbacks import EarlyStopping
@@ -59,6 +61,22 @@ def model():
     scaler.fit(X)
     X = scaler.transform(X)
 
+    from collections import Counter
+    from sklearn.ensemble import AdaBoostClassifier
+    from imblearn.under_sampling import InstanceHardnessThreshold
+    print(Counter(y))
+
+    undersample = InstanceHardnessThreshold(estimator=AdaBoostClassifier(),sampling_strategy={0:50000, 5:50000}, random_state=42)
+    #undersample = OneSidedSelection(n_neighbors=1, n_seeds_S=200, sampling_strategy={0:100000,5:100000})
+    X, y = undersample.fit_resample(X, y)
+    oversampledict = {1:50000, 2:5000, 3:50000, 4:50000}
+    sm = SMOTE(random_state=42, sampling_strategy=oversampledict)
+    X, y = sm.fit_resample(X, y)
+
+
+
+    print(Counter(y))
+
     labels = set(y)
     num_classes = len(labels)
     features = X.shape[1]
@@ -73,6 +91,7 @@ def model():
 
         for i in range(n_layers):
             model.add(Dense(units=neurons, activation='relu', kernel_initializer="glorot_normal"))
+            model.add(Dropout())
 
         model.add(Dense(units=num_classes, activation='softmax', kernel_initializer="glorot_normal"))
 
@@ -89,8 +108,8 @@ def model():
                        min_delta=0.0001
                        )
 
-    layers = [4]
-    neurons = [200]#, 60, 30]
+    layers = [3]
+    neurons = [90]#, 60, 30]
     hyperparameter_score_list = {}
     histories = {}
 
@@ -110,7 +129,7 @@ def model():
     bestresult = result[0][0]
     print(f"The best parameter configuration for Neural Network is: {bestresult[0]} (hidden) layers, {bestresult[1]} neurons")
 
-    '''
+
     fig, (ax1, ax2) = plt.subplots(2, 1)
     fig.set_size_inches(8, 8)
     ax1.plot(histories[bestresult].history['accuracy'])
@@ -127,7 +146,7 @@ def model():
     ax2.legend(['train', 'val'])
     plt.tight_layout()
     plt.show()
-    '''
+
 
     es = EarlyStopping(monitor='loss',
                        mode='min',
@@ -135,21 +154,63 @@ def model():
                        min_delta=0.0001
                        )
     performance = []
-    kf = KFold(n_splits = 2)
+    kf = KFold(n_splits = 5)
     for train_index, test_index in kf.split(X):
         X_train, X_test = X[train_index], X[test_index]
         y_cat_train, y_cat_test = y_cat[train_index], y_cat[test_index]
         bestmodel = NN_Builder(bestresult[0], bestresult[1])
         bestmodel.fit(X_train, y_cat_train, epochs=500, batch_size=16000, callbacks=[es])
-        performance.append(bestmodel.evaluate(X_test, y_cat_test))
+        performance.append(bestmodel.evaluate(X_test, y_cat_test)[1])
 
     print(f"performance length = {len(performance)}")
-    bestresult = np.mean(performance)
-    print(f"the best score isssss = {bestresult}")
+    print(f"performance = {performance}")
+    bestperformance = np.mean(performance)
+    print(f"The best score is equal to = {bestperformance}")
 
-    #bestmodel = NN_Builder(bestresult[0], bestresult[1])
-    #bestmodel.fit(X_train, y_train_cat, epochs=500, batch_size=16000, callbacks=[es])
-    #performance = bestmodel.evaluate(X_test, y_test_cat)
+
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+    y_train_cat = to_categorical(y_train, num_classes)
+    y_test_cat = to_categorical(y_test, num_classes)
+
+    bestmodel = NN_Builder(bestresult[0], bestresult[1])
+    bestmodel.fit(X_train, y_train_cat, epochs=500, batch_size=16000, callbacks=[es])
+    y_pred = bestmodel.predict(X_test)
+    y_pred = [np.argmax(i) for i in y_pred]
+
+    td = {0: "HBOND", 1: "IONIC", 2: "PICATION", 3: "PIPISTACK", 4: "SSBOND", 5: "VDW"}
+    y_test_1 = [td.get(item) for item in y_test]
+    y_pred_1 = [td.get(item) for item in y_pred]
+    precision = precision_score(y_test_1, y_pred_1, average=None)
+    precision = [i * 100 for i in precision]
+    recall = recall_score(y_test_1, y_pred_1, average=None)
+    recall = [i * 100 for i in recall]
+
+    target = ["HBOND", "IONIC", "PICATION", "PIPISTACK", "SSBOND", "VDW"]
+
+    X_axis = np.arange(len(target))
+
+    plt.bar(X_axis - 0.2, precision, 0.4, label='Precision')
+    plt.bar(X_axis + 0.2, recall, 0.4, label='Recall')
+
+    plt.xticks(X_axis, target)
+    plt.xlabel("Classes")
+    plt.ylabel("Performance")
+    plt.title("Precision-Recall graph")
+    plt.legend()
+    plt.show()
+
+    from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+    y_test_2 = [td.get(item) for item in y_test]
+    y_pred_2 = [td.get(item) for item in y_pred]
+    cm = confusion_matrix(y_test_2, y_pred_2, labels=target)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target)
+    disp.plot()
+    plt.show()
+
     #print(f"best accuracy with best model is equal to {performance[1]}")
 
     #from collections import Counter
